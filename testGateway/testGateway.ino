549bbb0bad0876fc40f6225124d51b6fd4d141f8
seksu <jarukit.suchat@gmail.com>
2017-04-16 11:50:52 +0700

    Add files via upload

---
+++ testGateway/testGateway.ino
@@ -0,0 +1,75 @@
+#include <ESP8266WiFi.h>
+
+////////////////////// CONFIG ///////////////////////
+
+const char* APNAME                = "Farm00";
+const char* APPASS                = "AdminFarm00";
+
+const char* ssid                  = "OHMAMP";
+const char* password              = "a029453745";
+
+const int farmId                  = 1;
+const int zoneId                  = 1;
+
+IPAddress ip_addr                 = {192, 168, 1, 200};
+IPAddress gateway                 = {192, 168, 1, 1};
+IPAddress subnet                  = {255, 255, 255, 0};
+
+////////////////////////////////////////////////////
+
+WiFiServer server(8888);
+WiFiClient client;
+
+void setup() {
+
+  Serial.begin(115200);
+  //Serial.println();
+  //Serial.print("Connecting to :");
+  //Serial.println(ssid);
+
+  //  WiFi.begin(ssid, password);
+  //  while (WiFi.status() != WL_CONNECTED) {
+  //    delay(200);
+  //    Serial.print(".");
+  //  }
+  //  WiFi.config(ip_addr, gateway, subnet);
+  //
+  //  Serial.println("WiFi connected");
+  //  Serial.print("IP address: ");
+  //  Serial.println(WiFi.localIP());
+
+  WiFi.softAP(APNAME, APPASS);
+  server.begin();
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  //////////////////////////////////
+
+  WiFiClient client;
+
+}
+
+void loop() {
+
+  client = server.available();
+  if (client) {
+    while (client.connected()) {
+      while (Serial.available()) {
+        String data = Serial.readStringUntil('\n');
+        client.println(data);
+        Serial.println(data);
+      }
+      while (client.available()) {
+        String data = client.readStringUntil('\n');
+        Serial.println(data);
+      }
+//      if(server.hasClient()){
+//        Serial.println("client disconnect");
+//        return;
+//      }
+    }
+      client.stop();
+      Serial.println("client disconnect");
+  }
+
+}
---
+++ testNode/testNode.ino
@@ -0,0 +1,73 @@
+#include <ESP8266WiFi.h>
+
+
+//////////////////// config ////////////////////////
+
+const char ssid[]     = "Farm00";
+const char password[] = "AdminFarm00";
+
+char server_ip[]      = "192.168.4.1";
+const uint16_t port   = 8888;
+
+const uint16_t nodeId = 1;
+
+///////////////////////////////////////////////////
+
+#define MAXRAND 5
+
+WiFiClient client;
+
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+//  WiFi.begin(ssid, password);
+//  while (WiFi.status() != WL_CONNECTED) {        // wait for connect to AP
+//    delay(200);
+//    Serial.print(".");
+//  }
+//  Serial.println();
+//  Serial.println("ready for fun");
+  // wait for connect to GateWay
+}
+void loop() {
+start :
+  long tempRand = random(1,MAXRAND);
+  Serial.print("RAND : ");
+  Serial.println(tempRand);
+  delay(1000 * tempRand);
+
+  WiFi.begin(ssid, password);
+  for (int i = 0 ; i <= 80 ; i++) {
+    if (i == 80)
+      goto start;
+    if (WiFi.status() == WL_CONNECTED)
+      break;
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println();
+
+  while (!client.connect(server_ip, port));
+  //Serial.print(".");
+
+  while (client.available()) {
+    String data = client.readStringUntil('\n');
+    Serial.println(data);
+  }
+  String temp = "Hello from node : ";
+  temp += nodeId;
+  temp += "\n";
+  client.print(temp);
+  Serial.println("Sended");
+  while (Serial.available()) {
+    String data = Serial.readStringUntil('\n');
+    Serial.println(data);
+    client.println(data);
+  }
+  client.stop();
+  WiFi.disconnect();
+  delay(5000);
+
+}
