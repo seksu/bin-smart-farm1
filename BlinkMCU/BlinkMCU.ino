3563885d354086c97d569e4117b4e034c4d463dc
seksu <jarukit.suchat@gmail.com>
2017-05-23 20:29:29 +0700

    Add files via upload

---
+++ BlinkMCU/BlinkMCU.ino
@@ -0,0 +1,39 @@
+#include <ESP8266WiFi.h>
+#define LED_BUILTIN D0
+#define LED2        D4
+//////////////////// config ////////////////////////
+
+const char ssid[]           = "OHMAMP";
+const char password[]       = "a029453745";
+
+///////////////////////////////////////////////////
+
+WiFiClient client;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  digitalWrite(LED2,HIGH);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
+  }
+  Serial.println();
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+}
+void loop() {
+    digitalWrite(LED2,HIGH);
+    delay(100);
+    digitalWrite(LED2,LOW);
+    delay(100);
+}
